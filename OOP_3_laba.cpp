618ad093b2b8dda1506d56e819f3f1d48816b138
Milyausha Asylova <asylova.03@mail.ru>
2023-03-13 16:40:32 +0500

    проблемы

--- OOP_3_laba.cpp
+++ OOP_3_laba.cpp
@@ -12,12 +12,6 @@ public:
 		a = rand();
 		str = to_string(a);
 	}
-	int get_a() {
-		return a;
-	}
-	string get_str() {
-		return str;
-	}
 };
 ostream& operator<<(ostream &out, const A &elem) {
 	return out << elem.str;
@@ -25,6 +19,7 @@ ostream& operator<<(ostream &out, const A &elem) {
 
 template <typename T>
 class Container {
+private:
 	T* arr;
 	int capacity;
 	int size;
@@ -32,7 +27,7 @@ class Container {
 	void increase_capacity() {
 		T* new_arr = new T[capacity * 2];
 
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i <= size; ++i) {
 			new_arr[i] = arr[i];
 		}
 
@@ -45,17 +40,16 @@ class Container {
 		capacity = size;
 		T* new_arr = new T[capacity];
 
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i <= size; ++i) {
 			new_arr[i] = arr[i];
 			delete[] arr;
 			arr = new_arr;
 		}	
 	}
 public: 
-	Container() :capacity(1), size(0) {
-		arr = new T[capacity];
-	}
+	Container() : capacity(1), size(0), arr(new T[capacity]) {}
 	Container(int capacity) : capacity(this->capacity), size(0), arr(new T[capacity]) {}
+
 	~Container() {
 		delete[]arr;
 	}
@@ -67,17 +61,17 @@ public:
 	void push_back(T val) {
 		if (size == capacity) increase_capacity();
 		arr[size] = val;
-		size++;
+		++size;
 	}
 
 	void pop_back() {
 		arr[size - 1] = 0;
-		size--;
+		++size;
 		if (size == (capacity / 2)) reduce_capacity();
 	}
 
 	int search(T val) {
-		for (int i = 0; i < size; i++) {
+		for (int i = 1; i <= size; ++i) {
 			if (arr[i] == val) {
 				return i;
 			}
@@ -103,11 +97,9 @@ public:
 			for (int i = size - 1; i >= index; i--) {
 				arr[i + 1] = arr[i];
 			}
-
 			arr[index] = val;
 			++size;
 		}
-		
 	}
 
 	 void set(int index, T val) {
@@ -119,31 +111,27 @@ public:
 		if (index <= size && index >= 0) 
 			return arr[index];
 	}
-
-	void print() {
-		for (int i = 1; i < size; ++i) {
-			cout << *arr[i];
-			cout << '\n';
-		}
-	}
 };
 
 int main() {
 	setlocale(LC_ALL, "rus");
 	Container<A*> arr;
 	clock_t start = clock();
-	for (int i = 0; i < 10000; ++i) {
+	int n = 10000;
+
+	for (int i = 0; i < n; ++i) {
 		arr.push_back(new A());
 	}
-	for (int i = 0; i < 1000; ++i) {
-		switch (rand() % 8)
+
+	for (int i = 0; i < n; ++i) {
+		switch (rand() % 7)
 		{
 		case 0:
 			arr.push_back(new A());
 			break;
 		case 1:
 			if (arr.get_size()) {
-				arr.remove(rand() % 10000);
+				arr.remove(rand() % arr.get_size());
 			}
 			break;
 		case 2:
@@ -152,26 +140,27 @@ int main() {
 			}
 			break;
 		case 3:
-			arr.get(rand() % 10000);
+			arr.get(rand() % arr.get_size());
+			break;
+		case 4:
+			arr.set(rand() % arr.get_size(), new A());
 			break;
 		case 5:
-			arr.set(rand() % 10000, new A());
+			arr.search(new A());
 			break;
 		case 6:
-			arr.search((new A()));
-			break;
-		case 7:
-			arr.push(rand() % 10000, new A());
+			arr.push(rand() % arr.get_size(), new A());
 			break;
 		}
-		
 	}
 	clock_t end = clock();
 	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
 
-	arr.print();
+	for (int i = 0; i < arr.get_size(); ++i) {
+		cout << *arr.get(i);
+		cout << '\n';
+	}
 
-	cout << endl;
+	cout << "\n";
 	cout << "Время работы - " << seconds << "секунд";
-	
 }
